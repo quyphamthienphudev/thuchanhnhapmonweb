6f1ac43d6619b09e9bfd57b2f88129c8e4631148
quyphamthienphudev <phamthienphuquy1007@gmail.com>
2026-05-15 18:03:09 +0700

    upload

--- detaicuoiky/js/script.js
+++ detaicuoiky/js/script.js
@@ -1,39 +1,28 @@
 // Khởi tạo EmailJS
 (function () {
-    emailjs.init("YDdwZaLx10yzVY25p");
+  emailjs.init("YDdwZaLx10yzVY25p");
 })();
 
-
 // Bắt sự kiện click button
-document
-    .getElementById("form")
-    .addEventListener("submit", function (event) {
+document.getElementById("form").addEventListener("submit", function (event) {
+  // Ngăn reload trang lại
+  event.preventDefault();
 
-        // Ngăn reload trang lại
-        event.preventDefault();
+  // Lấy email từ input
+  const email = document.getElementById("email").value;
 
-        // Lấy email từ input
-        const email = document.getElementById("email").value;
+  // Dữ liệu gửi đi
+  var templateParams = {
+    email: email,
+  };
 
-        // Dữ liệu gửi đi
-        var templateParams = {
-            email: email,
-        };
-
-        // Gửi email
-        emailjs.send(
-            "service_ifkakf3",
-            "template_v63iscl",
-            templateParams
-        ).then(
-            function (response) {
-                alert("Đăng ký thành công!");
-
-            },
-            function (error) {
-
-                alert("Gửi thất bại!");
-            }
-        );
-
-    });
+  // Gửi email
+  emailjs.send("service_ifkakf3", "template_v63iscl", templateParams).then(
+    function (response) {
+      alert("Đăng ký thành công!");
+    },
+    function (error) {
+      alert("Gửi thất bại!");
+    },
+  );
+});
